8cb93f3075d293b83be4486d215dfa2362c91634
DawnBreaker <tunganhngo207@gmail.com>
2025-01-10 22:58:32 +0700

    feat: update footer

--- src/app/layout/client/client.component.ts
+++ src/app/layout/client/client.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss',
 })
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    NzInputModule,
+    NzIconModule,
+    NzButtonModule,
+    NzGridModule,
+    RouterLink,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  activeSection?: string;
+  toggleSection(section: string): void {
+    this.activeSection = this.activeSection === section ? undefined : section;
+  }
+}
