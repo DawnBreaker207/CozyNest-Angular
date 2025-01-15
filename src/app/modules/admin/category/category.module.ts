58415db058a92cb5c36f0875c16b40e7ae320de1
DawnBreaker <tunganhngo207@gmail.com>
2025-01-15 15:06:21 +0700

    feat: update category list

--- src/app/modules/admin/category/category.module.ts
+++ src/app/modules/admin/category/category.module.ts
@@ -1,21 +1,42 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
 import { AddCategoryComponent } from './add/add.component';
 import { EditCategoryComponent } from './edit/edit.component';
 import { ListCategoryComponent } from './list/list.component';
-import { RouterModule, Routes } from '@angular/router';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
-import { NzTableModule } from 'ng-zorro-antd/table';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListCategoryComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'add',
+    component: AddCategoryComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'edit/:id',
+    component: EditCategoryComponent,
+    pathMatch: 'prefix',
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   imports: [
     CommonModule,
--- src/app/modules/admin/category/list/list.component.ts
+++ src/app/modules/admin/category/list/list.component.ts
@@ -1,20 +1,18 @@
 import { CategoryService } from '@/app/core/services/category.service';
 import { Category } from '@/app/shared/types/category';
+import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, takeUntil } from 'rxjs';
-import { NzDividerComponent } from 'ng-zorro-antd/divider';
-import { RouterLinkActive, RouterLink } from '@angular/router';
-import { ɵNzTransitionPatchDirective } from 'ng-zorro-antd/core/transition-patch';
+import { RouterLink } from '@angular/router';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
-import {
-  NzTableComponent,
-  NzTheadComponent,
-  NzTrDirective,
-  NzTableCellDirective,
-  NzThMeasureDirective,
-  NzTbodyComponent,
-} from 'ng-zorro-antd/table';
-import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { ɵNzTransitionPatchDirective } from 'ng-zorro-antd/core/transition-patch';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-category-list',
@@ -22,23 +20,27 @@ import { NgIf, NgFor, AsyncPipe } from '@angular/common';
   styleUrl: './list.component.scss',
   standalone: true,
   imports: [
-    NgIf,
-    NzTableComponent,
-    NzTheadComponent,
-    NzTrDirective,
-    NzTableCellDirective,
-    NzThMeasureDirective,
-    NzTbodyComponent,
-    NgFor,
+    NzTableModule,
     NzButtonComponent,
     ɵNzTransitionPatchDirective,
-    RouterLinkActive,
     RouterLink,
-    NzDividerComponent,
-    AsyncPipe,
+    NzIconModule,
+    NzSelectModule,
+    CommonModule,
+    NzSpaceModule,
+    NzTagModule,
+    NzPopconfirmModule,
+    NzInputModule,
   ],
 })
 export class ListCategoryComponent implements OnInit, OnDestroy {
+  columns = [
+    { title: 'Tên danh mục' },
+    { title: 'Ảnh danh mục' },
+    { title: 'Trạng thái hiển thị' },
+    { title: 'Ngày thêm' },
+    { title: 'Action' },
+  ];
   category$: Observable<Category[]> = new Observable();
   private unsubscribe = new Subject<void>();
 
