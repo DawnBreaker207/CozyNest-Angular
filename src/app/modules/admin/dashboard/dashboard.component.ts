860ea6514a25a2461c97421cbc909d378fc7791c
DawnBreaker <tunganhngo207@gmail.com>
2025-01-16 00:03:20 +0700

    feat: add dashboard

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -1,58 +1,82 @@
 import { OrdersService } from '@/app/core/services/orders.service';
 import { UsersService } from '@/app/core/services/users.service';
-import { CurrencyPipe } from '@angular/common';
+import { Order } from '@/app/shared/types/order';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { NzSelectModule } from 'ng-zorro-antd/select';
-import { map } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
+import { RecentOrderComponent } from './components/recent-order/recent-order.component';
+import { RevenueComponent } from './components/revenue/revenue.component';
+import { TopProductComponent } from './components/top-product/top-product.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink, NzSelectModule, CurrencyPipe],
+  imports: [
+    RouterLink,
+    NzSelectModule,
+    CurrencyPipe,
+    AsyncPipe,
+    RecentOrderComponent,
+    TopProductComponent,
+    RevenueComponent,
+    FormsModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
 })
 export class DashboardComponent {
-  ordersComplete: number = 0;
-  ordersCanceled: number = 0;
-  memberTotal: number = 0;
-  totalRevenue: number = 0;
+  orderSubject: BehaviorSubject<Order[]> = new BehaviorSubject<Order[]>([]);
+  selectedComponent: string = 'RecentOrder';
+
+  ordersCompleted$ = this.orderSubject
+    .asObservable()
+    .pipe(
+      map(
+        orders =>
+          orders.filter(order => order.status === 'Completed').length || 0
+      )
+    );
+  ordersCanceled$ = this.orderSubject
+    .asObservable()
+    .pipe(
+      map(
+        orders =>
+          orders.filter(order => order.status === 'Cancelled').length || 0
+      )
+    );
+  totalRevenue$ = this.orderSubject
+    .asObservable()
+    .pipe(
+      map(
+        orders =>
+          orders
+            .filter(order => order.status === 'Completed')
+            .reduce((sum: number, order) => sum + order.total_amount, 0) || 0
+      )
+    );
+
+  memberTotal$: number = 0;
+
   constructor(
     private orderService: OrdersService,
     private userService: UsersService
   ) {
+    this.orderService.getAll$.subscribe(orders =>
+      this.orderSubject.next(orders)
+    );
+
     this.userService
       .getAll$()
       .pipe(
         map(users => users.filter(user => user.role === 'member').length || 0)
       )
-      .subscribe(data => (this.memberTotal = data));
-    this.orderService.getAll$
-      .pipe(
-        map(
-          orders =>
-            orders.filter(order => order.status === 'Completed').length || 0
-        )
-      )
-      .subscribe(data => (this.ordersComplete = data));
-    this.orderService.getAll$
-      .pipe(
-        map(
-          orders =>
-            orders.filter(order => order.status === 'Cancelled').length || 0
-        )
-      )
-      .subscribe(data => (this.ordersCanceled = data));
-    this.orderService.getAll$
-      .pipe(
-        map(
-          orders =>
-            orders
-              .filter(order => order.status === 'Completed')
-              .reduce((sum: number, order) => sum + order.total_amount, 0) || 0
-        )
-      )
-      .subscribe(data => (this.totalRevenue = data));
+      .subscribe(data => (this.memberTotal$ = data));
+  }
+
+  handleComponentChange(value: string) {
+    this.selectedComponent = value;
   }
 }
