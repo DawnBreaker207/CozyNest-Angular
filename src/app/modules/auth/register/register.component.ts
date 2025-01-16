d693d91a2f6121edf24aa91e93294e10cc0c2a80
DawnBreaker <tunganhngo207@gmail.com>
2025-01-16 15:58:23 +0700

    feat: add register page

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NzFormModule,
+    NzCheckboxModule,
+    FormsModule,
+    NzButtonModule,
+    NzInputModule,
+    NzIconModule,
+  ],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  passwordVisible = false;
+  password?: string;
+}
